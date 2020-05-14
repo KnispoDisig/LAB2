840ddb5da5fb25c6a0fcf75318b785fc8d6f9e75
Mark Karavashkin <fell2001@mail.ru>
2020-05-14 12:52:11 +0300

    cmake added

---
+++ cmake-build-debug/CMakeFiles/theFirstApp.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/fell2/CLionProjects/theFirstApp/Complex/Complex.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/Complex/Complex.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/DiagonalMatrix.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/DiagonalMatrix.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/Sequence/ArraySequence/ArraySequence.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/Sequence/ArraySequence/ArraySequence.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/Sequence/ArraySequence/DynamicArray/DynamicArray.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/Sequence/ArraySequence/DynamicArray/DynamicArray.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/Sequence/ListSequence/LinkedList/LinkedList.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/Sequence/ListSequence/LinkedList/LinkedList.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/Sequence/ListSequence/LinkedListSequence.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/Sequence/ListSequence/LinkedListSequence.cpp.obj"
+  "C:/Users/fell2/CLionProjects/theFirstApp/main.cpp" "C:/Users/fell2/CLionProjects/theFirstApp/cmake-build-debug/CMakeFiles/theFirstApp.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/theFirstApp.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/theFirstApp.dir/Complex/Complex.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/DiagonalMatrix.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/Sequence/ArraySequence/ArraySequence.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/Sequence/ArraySequence/DynamicArray/DynamicArray.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/Sequence/ListSequence/LinkedList/LinkedList.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/Sequence/ListSequence/LinkedListSequence.cpp.obj"
+  "CMakeFiles/theFirstApp.dir/main.cpp.obj"
+  "libtheFirstApp.dll.a"
+  "theFirstApp.exe"
+  "theFirstApp.exe.manifest"
+  "theFirstApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/theFirstApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
